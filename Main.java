ca06e25074d5f199cbda51749ba66f586339912d
balez4464 <71140813+balez4464@users.noreply.github.com>
2020-10-05 11:58:20 -0400

    Update Main.java
    had to manually commit it

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program asks the user for a number nad shows them all thier factors
+ * @author Zachary Balean
  */
 public class Main {
 
@@ -9,7 +11,35 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates Scanner
+    Scanner input = new Scanner(System.in);
+
+    //asks user to put in positive integer
+    System.out.println("Please enter a positive integer to determine its factors:");
+    int usersNumber = input.nextInt();
+
+    //while loop to make them enter a positive int
+    while(usersNumber <= 0){
+      System.out.println("Please enter a positive integer to determine its factors:");
+      //allows them to enter new number
+      usersNumber = input.nextInt();
+    }
+
+    //delcares interger for the dividers and remainders
+    int divider = 1;
+    int remainder = 0;
+
+    //while loop to determine factors
+    System.out.println("The factors of " + usersNumber + " are:");
+    while(divider <= usersNumber){
+      remainder = usersNumber % divider;
+      //to print out the divider number as a factor
+      if(remainder == 0){
+        System.out.println(divider);
+      }
+      //adds 1 to the divider until it reaches users number
+      divider = divider + 1;
+    } 
     
   }
 }
